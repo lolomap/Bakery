3a1079cdf50cb331cc79cddd404dfa771042744f
Пригарин Олег Юрьевич <lolomap07@yandex.ru>
2025-11-27 22:45:53 +0300

    Isometry base player

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public static class Helper
+{
+	private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+
+	public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, Controls.IPlayerActions
+{
+	public float Speed;
+	public Quaternion MovementRotation = Quaternion.Euler(0f, 45f, 0f);
+
+	private Vector3 _direction;
+
+	private CharacterController _controller;
+
+	private void Awake()
+	{
+		_controller = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		_controller.Move(_direction * (Speed * Time.deltaTime));	
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		Vector2 inputDirection = context.ReadValue<Vector2>();
+		
+		Matrix4x4 isoMatrix = Matrix4x4.Rotate(MovementRotation);
+		_direction = isoMatrix.MultiplyVector(new(inputDirection.x, 0f, inputDirection.y));
+	}
+
+	public void OnLook(InputAction.CallbackContext context)
+	{
+		
+	}
+
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		
+	}
+}
